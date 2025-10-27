2c476c111a3ebbdacd8b51fba5dc4eda1f8838ed
simonbuller <simon.buller.s@csi.cl>
2025-10-27 13:11:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 # Título de la app
-st.title("¡Mi primera app!")
-
+st.title("¿QUE MEDICMENTO QUIERES VER HOY?")
 # Texto simple
 st.write("Hola, soy [TU NOMBRE] y esta es mi primera aplicación con Streamlit.")
 
