08799ab6d9510e36a6269e2caad696aca2ba5e01
simonbuller <simon.buller.s@csi.cl>
2025-10-27 13:24:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ st.title("¿QUE MEDICMENTO QUIERES VER HOY?")
 # Texto simple
 st.header("sigue aca:")
 
-st.write("Hola, soy [TU NOMBRE] y esta es mi primera aplicación con Streamlit.")
+st.write("Hola, soy [TU NOMBRE] y esta es mi primera aplicación con Streamlit.") 
+
 
 # Un input interactivo
 cuando = st.number_input("¿en cuantos dias lo quieres?", min_value=0, max_value=10)
 
 opcion = st.selectbox("Elige cuantos medicamentos quieres", ["1", "2", "3"])
 
-
+if st.button("Click aquí"):st.write("¡invalido!")
 # Un botón
 if st.button("Presiona aquí"):
     st.balloons()  # Animación de globos
